774c7f09e905fd233f3f9d287e256bae67d66f36
arielmell0 <arielmello@rede.ulbra.br>
2022-05-26 01:18:01 -0300

    feat: usando DOM para renderizar o quantity na tela

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -3,12 +3,12 @@ const Counter = () => {
 
     const upQuantity = () => {
         quantity = quantity + 1;
-        console.log(quantity);
+        document.getElementById("counter-box").innerHTML = quantity
     } 
 
     return (
         <>
-            <h1>{quantity}</h1>
+            <h1 id="counter-box">{quantity}</h1>
             <button onClick={upQuantity}>Aumentar</button>
         </>
     );
