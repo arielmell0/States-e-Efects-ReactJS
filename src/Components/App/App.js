f98ce2dd7c23795c48ce84d1bd6e1afa341cb71b
arielmell0 <arielmello@rede.ulbra.br>
2022-05-26 01:17:03 -0300

    feat: aumentando a quantidade
    quantidade aumenta na memória mas não renderiza na tela

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from '../Counter/Counter';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Counter />
+    </>
   );
 }
 
--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,8 +1,15 @@
 const Counter = () => {
+    let quantity = 10;
+
+    const upQuantity = () => {
+        quantity = quantity + 1;
+        console.log(quantity);
+    } 
+
     return (
         <>
-            <h1>1</h1>
-            <button>Aumentar</button>
+            <h1>{quantity}</h1>
+            <button onClick={upQuantity}>Aumentar</button>
         </>
     );
 };
