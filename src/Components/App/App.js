830d3f3a2e4bc8d6c9af030e0013b738020d70f8
arielmell0 <arielmello@rede.ulbra.br>
2022-05-27 00:59:14 -0300

    feat: utilizando componente statefull
    utilizando useState para atualizar e renderizar o valor da variavel na tela

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from '../Counter/Counter';
+import Smartcounter from '../Smartcounter/Smartcounter';
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <Counter />
+      <Smartcounter />
     </>
   );
 }
---
+++ src/Components/Smartcounter/Smartcounter.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const Smartcounter = () => {
+    // retorna um vetor
+    // 1. variavel stateful
+    // 2. função amarrada a essa variavel que atualiza esse valor
+    const [ quantity, upQuantity ] = useState(1);
+
+    return (
+        <>
+            <h1>{quantity}</h1>
+            <button onClick={() => upQuantity(quantity + 1)}>Aumentar</button>
+        </>
+    )
+}
+
+export default Smartcounter;
